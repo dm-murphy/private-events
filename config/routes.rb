868e21bc58ca0180bfe63d6f5293f0e6d1c02caf
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-08-17 16:10:57 -0400

    Fix singular user to plural users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   root "events#index"
 
-  get 'events/index'
-  get 'events/create'
+  resources :events, only: [:index, :show]
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/user/index"
+      get "/users/index"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/user/show"
+      get "/users/show"
       expect(response).to have_http_status(:success)
     end
   end
