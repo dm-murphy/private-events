f5fd0435bdf4b06b61d49a9a6601777cd6a87e5b
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-08-18 13:21:48 -0400

    Refactor request specs to green

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,20 @@
+# spec/requests/users_spec.rb
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/users/index"
+      get "/users"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/users/show"
+      user = FactoryBot.create(:user)
+
+      get users_path(user)
       expect(response).to have_http_status(:success)
     end
   end
-
 end
