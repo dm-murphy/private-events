2bfa8fd08e58afa4fab15ab5ac929583d778fa5b
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-08-20 00:10:43 -0400

    Add new associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
+  belongs_to_many :attended_events, class_name: 'Event', foreign_key: 'event_id'
 end
