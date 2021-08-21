a40cfd7a63f59ed0e8f4672da646243ae947bcbb
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-08-21 12:45:46 -0400

    Change #create and params

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,29 @@
 class InvitationsController < ApplicationController
+  def index
+    @invitations = Invitation.all
+  end
+
+  def show
+    @invitation = Invitation.find(params[:id])
+  end
+
+  def new
+    @invitation = Invitation.new
+  end
+
+  def create
+    @invitation = current_user.invitations.build(invitation_params)
+
+    if @invitation.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def invitation_params
+      params.permit(:event_id, :user_id)
+    end
 end
