1772e6e287fa135a12b9a56b2e2c508397caf43e
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-08-21 12:46:33 -0400

    Add invitation form to Events#show view

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  has_many :attendees, through: :invitations, source: :creator
+  has_many :attendees, through: :invitations, class_name: 'User', foreign_key: 'user_id'
 end
