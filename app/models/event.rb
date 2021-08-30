9d4e9a2befb79f038e7bdcfbf24409cc6b59a397
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-08-30 10:54:06 -0400

    Add attendee and attended_event names and sources

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :invitations
-  has_many :users, through: :invitations
+  has_many :attendees, through: :invitations, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :events
   has_many :invitations
-  has_many :events, through: :invitations
+  has_many :attended_events, through: :invitations, source: :event
 end
