d17453b914322d650cd4483b17a59bc134b1fe42
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-09-12 21:34:10 -0400

    Separate past/upcoming events on Events#index w/class methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :invitations
   has_many :attendees, through: :invitations, source: :user
+
+  def Event.past
+    where("date < ?", Time.current)
+  end
+
+  def Event.upcoming
+    where("date > ?", Time.current)
+  end
 end
